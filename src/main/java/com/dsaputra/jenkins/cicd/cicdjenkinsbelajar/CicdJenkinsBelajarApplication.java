7cbdef7557de0d9cafa70aae9fcd9e813ed85806
Dedi Dwi Saputra <DEDID1@intra.excelcom.co.id>
2019-04-27 03:56:37 +0700

    New Upload

---
+++ src/main/java/com/dsaputra/jenkins/cicd/cicdjenkinsbelajar/CicdJenkinsBelajarApplication.java
@@ -0,0 +1,13 @@
+package com.dsaputra.jenkins.cicd.cicdjenkinsbelajar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdJenkinsBelajarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdJenkinsBelajarApplication.class, args);
+	}
+
+}
